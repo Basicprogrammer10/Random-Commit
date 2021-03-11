293530b58aa6567cf25d6abb3361bb7031ed530e
Connor Slade <turtleslade@gmail.com>
2021-03-11 10:17:46 -0500

    🛫 Remove Extra Space

--- getMessages.py
+++ getMessages.py
@@ -6,18 +6,15 @@ index = 100000
 outFile = 'out.txt'
 uri = 'http://whatthecommit.com/index.txt'
 
-
 ######### FUNCTIONS #########
 def colored(text, color):
     colorCodes = {'black': '30', 'red': '31', 'yellow': '33', 'green': '32', 'blue': '34', 'cyan': '36',
                   'magenta': '35', 'white': '37', 'gray': '90', 'reset': '0'}
     return '\033[' + colorCodes[str(color).lower()] + 'm' + str(text) + "\033[0m"
 
-
 def DebugPrint(Catagory, Text, Color, end = "", start = ""):
     print(start + colored('['+datetime.now().strftime("%H:%M:%S")+'] ', 'yellow') +
           colored('['+Catagory+'] ', 'magenta')+colored(Text, Color), end=end)
-
 
 def getMessages(index, uri):
     messages = []
@@ -28,7 +25,6 @@ def getMessages(index, uri):
         messages.append(working)
         DebugPrint('Request', f"{i}/{index} ~~ {round(i / index * 100, 3)}%", 'cyan', end="\r")
     return messages
-
 
 def saveMessages(file, messages):
     DebugPrint('Save', f"Saveing {len(messages)} Messages!", 'cyan', start="\n", end="\n")
@@ -41,13 +37,11 @@ def saveMessages(file, messages):
         except:
             pass
 
-
 def main():
     DebugPrint('Main', 'Starting...\n', 'green')
     messages = getMessages(index, uri)
     saveMessages(outFile, messages)
     DebugPrint('Main', 'Finished...', 'red')
-
 
 if __name__ == '__main__':
     main()
