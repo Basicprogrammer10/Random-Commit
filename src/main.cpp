492a539ee84b6d07b47fde5a15d4c8ec31f68ccc
Connor Slade <turtleslade@gmail.com>
2021-03-21 18:11:48 -0400

    🎨 Add Color :P

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <ctime>
+//#include <windows.h>
+
 #include "messages.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
+    //DWORD l_mode;
+    //HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    //GetConsoleMode(hStdout, &l_mode);
+    //SetConsoleMode(hStdout, l_mode | 0x0004 | 0x0008);
     string gitArgs = "-a ";
 
     if (argc > 1){
@@ -20,7 +26,7 @@ int main(int argc, char **argv) {
 
     string msg = "git commit " + gitArgs + "-m \"" + commitMessages::messages[RandIndex] + "\"";
 
-    cout << "[*] Running: " << msg << endl;
-    system(msg.c_str());
+    cout << "\033[32m[*] Running: \033[34m" << msg << "\033[0m" << endl;
+    //system(msg.c_str());
     return 0;
 }
